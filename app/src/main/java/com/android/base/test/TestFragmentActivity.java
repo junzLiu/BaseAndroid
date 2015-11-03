22883813d8fb9530180ee2c2e0b051fb67440568
junzLiu <stylite1991@gmail.com>
2015-11-03 16:42:40 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/android/base/test/TestFragmentActivity.java
@@ -0,0 +1,42 @@
+package com.android.base.test;
+
+import android.base.com.baseandroid.R;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.widget.FrameLayout;
+
+import com.android.base.app.activity.BaseActivity;
+
+
+/**
+ * @author Mark
+ * @version 1.0
+ * @module
+ * @title
+ * @function
+ * @createTime 2015年10月8日 上午10:04:46
+ */
+
+public class TestFragmentActivity extends BaseActivity {
+
+
+    Fragment mFragment;
+    FrameLayout mContent;
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_test;
+    }
+
+    @Override
+    protected void init() {
+        mFragment = (Fragment) getIntent().getSerializableExtra(TestUIActivity.EXTRA_FRAGMENT);
+        mContent = (FrameLayout) findViewById(R.id.content);
+        if (mFragment == null) {
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().add(R.id.content, mFragment).commit();
+        }
+    }
+
+}
