cd0b4d14a1b3e6221ec0eb02f5711412b31be2eb
junzLiu <stylite1991@gmail.com>
2015-12-29 18:03:52 +0800

    Rejected commit: Default

--- app/src/main/java/com/android/base/app/activity/BaseActivity.java
+++ app/src/main/java/com/android/base/app/activity/BaseActivity.java
@@ -1,9 +1,13 @@
 package com.android.base.app.activity;
 
+import android.app.ActionBar;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SpinnerAdapter;
 
 /**
  * Created by dell on 2015/11/2.
@@ -17,6 +21,67 @@ public abstract class BaseActivity extends FragmentActivity {
         mRootView = LayoutInflater.from(this).inflate(getLayoutId(), null);
         init();
         setContentView(getLayoutId());
+        getActionBar().setListNavigationCallbacks(new SpinnerAdapter() {
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public int getItemViewType(int position) {
+                return 0;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        }, new ActionBar.OnNavigationListener() {
+            @Override
+            public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+                return false;
+            }
+        });
     }
 
     /**
